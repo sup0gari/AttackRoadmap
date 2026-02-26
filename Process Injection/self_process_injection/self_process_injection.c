9bbb2a27e7025731368da65fb542af25ec995e3d
sup0gari <sup0garixlolz@gmail.com>
2026-02-26 17:22:57 +0900

    execute modified

--- Process Injection/self_process_injection/self_process_injection.c
+++ Process Injection/self_process_injection/self_process_injection.c
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-BYTE* convert(HANDLE data_handle, DWORD* data_size) {
+BYTE* read_file(HANDLE data_handle, DWORD* data_size) {
     *data_size = GetFileSize(data_handle, NULL);
     if (*data_size == INVALID_FILE_SIZE) return NULL;
 
@@ -17,7 +17,23 @@ BYTE* convert(HANDLE data_handle, DWORD* data_size) {
 }
 
 void execute(BYTE* data) {
+    // Check magic number
+    PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)data;
+    if (dos->e_magic != IMAGE_DOS_SIGNATURE) return;
 
+    // Check data size
+    PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)(data + dos->e_lfanew);
+    PVOID base = VirtualAlloc(NULL, nt->OptionalHeader.SizeOfImage, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (!base) return;
+
+    // Copy section table (DOS header, NT header, Section table)
+    memcpy(base, data, nt->OptionalHeader.SizeOfHeaders);
+    PIMAGE_SECTION_HEADER section = IMAGE_FIRST_SECTION(nt);
+
+    // Copy section data
+    for (int i = 0; i < nt->FileHeader.NumberOfSections; i++) {
+        memcpy((BYTE*)base + section[i].VirtualAddress, (BYTE*)data + section[i].PointerToRawData, section[i].SizeOfRawData);
+    }
 }
 
 int main() {
@@ -25,7 +41,7 @@ int main() {
     if (handle == INVALID_HANDLE_VALUE) return 1;
 
     DWORD exe_size = 0;
-    BYTE* exe = convert(handle, &exe_size);
+    BYTE* exe = read_file(handle, &exe_size);
 
     CloseHandle(handle);
 
