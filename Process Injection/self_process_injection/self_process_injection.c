a8ed23dd1335c6d944df279b4bb19694f491b0ae
sup0gari <sup0garixlolz@gmail.com>
2026-02-26 17:04:54 +0900

    read_file done

--- Process Injection/self_process_injection/self_process_injection.c
+++ Process Injection/self_process_injection/self_process_injection.c
@@ -1,11 +1,23 @@
 #include <windows.h>
 
-BYTE* reassemble(HANDLE data_handle, DWORD* data_size) {
+BYTE* convert(HANDLE data_handle, DWORD* data_size) {
+    *data_size = GetFileSize(data_handle, NULL);
+    if (*data_size == INVALID_FILE_SIZE) return NULL;
 
+    BYTE* buffer = (BYTE*)HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, *data_size);
+    if (buffer == NULL) return NULL;
+
+    DWORD bytes_read;
+    if (!ReadFile(data_handle, buffer, *data_size, &bytes_read, NULL) || bytes_read != *data_size) {
+        HeapFree(GetProcessHeap(), 0, buffer);
+        return NULL;
+    }
+
+    return buffer;
 }
 
 void execute(BYTE* data) {
-    
+
 }
 
 int main() {
@@ -13,7 +25,7 @@ int main() {
     if (handle == INVALID_HANDLE_VALUE) return 1;
 
     DWORD exe_size = 0;
-    BYTE* exe = reassemble(handle, &exe_size);
+    BYTE* exe = convert(handle, &exe_size);
 
     CloseHandle(handle);
 
